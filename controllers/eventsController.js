383e729b620af7bc3810f77787439213101ff7f0
Aira Mel <aira.souza@sou.inteli.edu.br>
2025-05-26 13:44:11 -0300

    fix events

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -1,6 +1,6 @@
 const Events = require('../models/eventsModel');
 
-// Listar eventos
+// Listar todos os eventos
 exports.index = async (req, res) => {
   try {
     const events = await Events.findAll();
@@ -14,25 +14,25 @@ exports.index = async (req, res) => {
   }
 };
 
-// Criar uma nova inscrição
+// Criar um novo evento
 exports.create = async (req, res) => {
-  const { users_id, events_id, date, hour, status } = req.body;
+  const { title, type, description, photo, locations_id } = req.body;
 
   try {
-    await Events.create({ users_id, events_id, date, hour, status });
+    await Events.create({ title, type, description, photo, locations_id });
     res.status(200).json({message: "Evento criado"});
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-// Atualizar dados da inscrição
+// Atualizar dados do evento
 exports.update = async (req, res) => {
   const { id } = req.params;
-  const { users_id, events_id, date, hour, status } = req.body;
+  const { title, type, description, photo, locations_id } = req.body;
 
   try {
-    const event = await Events.update(id, { users_id, events_id, date, hour, status });
+    const event = await Events.update(id, { title, type, description, photo, locations_id });
     if (req.headers.accept && req.headers.accept.includes('application/json')) {
       res.status(200).json(event);
     } else {
@@ -43,14 +43,14 @@ exports.update = async (req, res) => {
   }
 };
 
-// Deletar inscrição
+// Deletar evento
 exports.delete = async (req, res) => {
   const { id } = req.params;
 
   try {
     const event = await Events.delete(id);
     if (req.headers.accept && req.headers.accept.includes('application/json')) {
-      res.status(200).json({ message: 'event deleted successfully', event });
+      res.status(200).json({ message: 'Event deleted successfully', event });
     } else {
       res.redirect('/events');
     }
